2c44ba2ea9eb780d5be2f070a6a9a891aaad6a0e
MiklKanikl <134088130+MiklKanikl@users.noreply.github.com>
2026-05-19 18:59:51 +0200

    basic working connection (no updating)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import random
 
 app = Flask(__name__)
@@ -6,21 +6,28 @@ data = {}
 
 @app.route("/api/create_token/")
 def create_token():
-    t = random.randint(1000, 9999)
-    data[t] = {"nodes": [], "edges": []}
-    return t
+    t = 0
+    while t == 0 or t in data.keys():
+        t = random.randint(1000, 9999)
 
-@app.route("/api/get_data/")
+    data[t] = {"nodes": [], "edges": []}
+    return str(t)
+
+@app.route("/api/get_data/", methods=['Post'])
 def get_data():
     t = request.args.get("key")
-    return data[t]
+    if t in data.keys():
+        return jsonify(data[t])
+    else:
+        return "key does not exist", 400
 
-@app.route("/api/send_data/")
+@app.route("/api/send_data/", methods=['Post'])
 def send_data():
     c_request = request.get_json()
     c_data = c_request["data"]
     c_key = c_request["key"]
     data[c_key] = c_data
+    return "success", 200
 
 if __name__ == '__main__':
     app.run()
